a50e2f8d9df377d3577a182b49b94bea4e96b51d
Justin Rice <jsrice7391@gmail.com>
2018-01-08 11:46:00 -0500

    API calls work

--- controller/mainCont.js
+++ controller/mainCont.js
@@ -1,11 +1,29 @@
 const db = require("../models");
 
-
-
+console.log(db.Burger)
 
 module.exports = function(app) {
     app.get("/", function(req, res) {
-        res.render("index");
+        db.Burger.findAll({}).then(function(burgers) {
+            res.render("index", { burgers: burgers });
+        })
 
     })
+
+    app.get("/api/burgers/:id", function(req, res) {
+        db.Burger.findById(req.body.id).then(function(burgers) {
+            res.json(burgers);
+        })
+    })
+
+    app.post("/api/burgers", function(req, res) {
+        db.Burger.create({
+            burger_name: req.body.burger_name,
+            complete: req.body.complete
+        }).then(function(dbTodo) {
+            // We have access to the new todo as an argument inside of the callback function
+            res.render("index", { burgers: dbTodo });
+
+        });
+    });
 }
--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
-    var Burger = sequelize.define("burger", {
+    var Burger = sequelize.define("Burger", {
         burger_name: DataTypes.STRING,
         complete: DataTypes.BOOLEAN
     })
