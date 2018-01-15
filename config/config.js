5718aa17310a6699df789cb30eb3d911e9546275
Justin Rice <jsrice7391@gmail.com>
2018-01-15 13:14:54 -0500

    Works

--- config/config.js
+++ config/config.js
@@ -21,7 +21,7 @@ module.exports = {
         database: "seq_burger",
         host: "127.0.0.1",
         dialect: "mysql",
-        use_env_variable: JAWSDB_URL
+        use_env_variable: process.env.JAWSDB_URL
     }
 
 }
